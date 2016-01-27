393b7356e2c6bf1277e86fcd27dd54ba9d0b0b7a
Muhammad Aamir Waseem <maniazi83@gmail.com>
2016-01-27 19:46:26 +0500

    Updated DNN Development Template (CRs)

--- Plugins/DNN/DevelopementTemplate/VisualStudio_2013/VSIX/VB-Template/View.ascx.vb
+++ Plugins/DNN/DevelopementTemplate/VisualStudio_2013/VSIX/VB-Template/View.ascx.vb
@@ -6,6 +6,8 @@
 ' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
 ' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ' DEALINGS IN THE SOFTWARE.
+' This is an extended version of Christoc's DotNetNuke Module and Theme Development Template. (http://christoctemplate.codeplex.com)
+' Copyright under the license http://christoctemplate.codeplex.com/license
 ' 
 Imports DotNetNuke
 Imports DotNetNuke.Entities.Modules.Actions
